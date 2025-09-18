84681beefd8639986f09ec44d7056b0bc4cb5e89
Ashik <ashikurashik20999@gmail.com>
2025-09-19 03:33:14 +0600

    Visited toggle is added

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,17 +1,20 @@
-import React, { use } from 'react';
-import Country from './Country';
+import React, { use } from "react";
+import Country from "./Country";
+import styles from './Countries.module.css';
 
-const Countries = ({CountriesPromise}) => {
-    const countriesData = use(CountriesPromise);
-    const countries = countriesData.countries;
-    return (
-        <div>
-            <h3>In the Countries {countries.length} </h3>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
-        </div>
-    );
+const Countries = ({ CountriesPromise }) => {
+  const countriesData = use(CountriesPromise);
+  const countries = countriesData.countries;
+  return (
+    <div>
+      <h3>In the Countries {countries.length} </h3>
+      <div className={styles.countriesContainer}>
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Country.module.css';
 
 const Country = ({country}) => {
-    console.log(country.name);
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () =>{
+        if(visited){
+            setVisited(false);
+        }
+        else{
+            setVisited(true);
+        }
+
+        // setVisited(visited ? false : true)
+
+        // setVisited(!visited)
+    }
     return (
-        <div>
+        <div className={`${styles.country} ${visited && styles.countryVisited }`}>
+            <img className={styles.countryImg} src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
+            <p>Population: {country.population.population} </p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? <span className={styles.span}>Large Country</span> : <span className={styles.span}>Small Country</span> } </p>
+            <button onClick={handleVisited}>
+                {
+                    visited? "Visited" : "Not Visited"
+                }
+            </button>
         </div>
     );
 };
