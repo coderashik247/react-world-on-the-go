1c75edc75f795e76b5686500d388213c70bd0e19
Ashik <ashikurashik20999@gmail.com>
2025-09-20 00:33:13 +0600

    add flag

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -25,8 +25,15 @@ const Countries = ({ CountriesPromise }) => {
   const [addFlag, setAddFlag] = useState([]);
 
   const handleAddFlag = (country) => {
-    console.log("Clicked Flag", country);
-  }
+    const alreadyAddFlag = addFlag.find((c) => (c.cca3 === country.cca3));
+
+    if (alreadyAddFlag) {
+      alert(" ❌ Already Exits !!");
+    } else {
+      const newAddFlag = [...addFlag, country];
+      setAddFlag(newAddFlag);
+    }
+  };
 
   const countriesData = use(CountriesPromise);
   const countries = countriesData.countries;
@@ -36,16 +43,22 @@ const Countries = ({ CountriesPromise }) => {
       <h4>Visited Countries = {visitedCountries.length}</h4>
       <ol>
         {visitedCountries.map((country) => (
-          <li> {country.name.common} </li>
+          <li>  {country.name.common} </li>
         ))}
       </ol>
+      <h4>Visited Countries Flag = {addFlag.length}</h4>
+      <div>
+        {addFlag.map((flag, index) => (
+          <img key={index} className={styles.addFlagImg} src={flag.flags.flags.png} ></img>
+        ))}
+      </div>
       <div className={styles.countriesContainer}>
         {countries.map((country) => (
           <Country
             key={country.cca3.cca3}
             country={country}
             handleVisitedCountries={handleVisitedCountries}
-            handleAddFlag = {handleAddFlag}
+            handleAddFlag={handleAddFlag}
           ></Country>
         ))}
       </div>
